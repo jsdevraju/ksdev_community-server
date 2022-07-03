58ceb6b221cb6e715d2ca69361b809dd5c3b3016
jsdevraju <islamrajulearn.dev@gmail.com>
2022-07-03 09:56:59 +0600

    fix token issue

--- middleware/authSocket.js
+++ middleware/authSocket.js
@@ -1,6 +1,5 @@
 import jwt from 'jsonwebtoken';
 import catchAsyncError from "./catchAsyncError.js";
-import ErrorHandler from "../utils/errorHandler.js";
 import User from '../models/userModel.js'
 
 export const verifyTokenSocket = catchAsyncError(async(socket, next) => {
@@ -8,7 +7,7 @@ export const verifyTokenSocket = catchAsyncError(async(socket, next) => {
     
   if (!token)
     return next(
-      new ErrorHandler("Invalid authorization token. please try again", 400)
+      new Error("Invalid authorization token. please try again")
     );
 
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
